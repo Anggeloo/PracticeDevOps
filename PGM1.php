5acaf03f0f26608803b31fd899505c11a1f14d7f
Anggelo <95989098+Anggeloo@users.noreply.github.com>
2024-12-02 18:28:29 -0500

    add-update-pgm1

--- PGM1.php
+++ PGM1.php
@@ -5,6 +5,6 @@
     </head>
     <body>
         <h1><?php echo "Hello World with PHP"; ?></h1>
-        <?php echo '<p>I am Anggelo Cadena.</p>'; ?>
+        <?php echo '<p>I am Anggelo Cadena Tercero.</p>'; ?>
     </body>
 </html>
